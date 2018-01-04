433bba46f19f0b528418174eb59195f8f305b840
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-01-03 23:11:13 -0300

    add a relationship belongs_to Kind

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,19 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
-  
-  def author
-    'Jackson Pires'
-  end
+  belongs_to :kind #, optional: true
 
-  def as_json(options={})
-    super(methods: :author, root: true)
-  end 
+#  def author
+#    'Jackson Pires'
+#  end
+
+#  def kind_description
+#    self.kind.description
+#  end
+
+#  def as_json(options={})
+#    super(
+#      root: true,
+#      methods: [:kind_description, :author],
+#      include: { kind: { only: :description } }
+#      )
+#  end
 end
