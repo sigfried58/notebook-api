2062029f1b9d23fb4c388bf63cfc42da762647a8
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-01-06 20:51:38 -0300

    Integration AMS and Links

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,10 +1,23 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate
+  attributes :id, :name, :email, :birthdate #, :author
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:related) { kind_url(object.id) }
+  end
   has_many :phones
   has_one :address
 
+  # link(:self) { contact_url(object.id) }
+  # link(:kind) { kind_url(object.id) }
+
+  # def author
+  #   'Jimmy Ibanez'    
+  # end
+
+  meta do
+    { author: 'Jimmy ibanez' }
+  end 
+  
   def attributes(*args)
     h = super(*args)
     # es ---> h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
