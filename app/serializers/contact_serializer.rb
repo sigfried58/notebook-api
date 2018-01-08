7a863e6fb754f2e8a3c4d28b986af6d7edecf2c1
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-01-08 13:01:56 -0300

    Relationships and routes kind (has_many)

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -4,7 +4,10 @@ class ContactSerializer < ActiveModel::Serializer
   belongs_to :kind do
     link(:related) { contact_kind_url(object.id) }
   end
-  has_many :phones
+  has_many :phones do
+    link(:related) { contact_phones_url(object.id) }
+  end
+
   has_one :address
 
   # link(:self) { contact_url(object.id) }
