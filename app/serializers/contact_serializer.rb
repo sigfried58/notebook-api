3454dd67af496216d82c415b4c0cc310a484b115
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-01-08 14:40:52 -0300

    Relationships and routes kind (has_one)

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -8,7 +8,9 @@ class ContactSerializer < ActiveModel::Serializer
     link(:related) { contact_phones_url(object.id) }
   end
 
-  has_one :address
+  has_one :address do
+    link(:related) { contact_address_url(object.id) }
+  end
 
   # link(:self) { contact_url(object.id) }
   # link(:kind) { kind_url(object.id) }
