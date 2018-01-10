1c0cfb0dcaefab03914aeedcc69dfb66c1e39530
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-01-10 15:22:10 -0300

    Versioning HTTP Custom header with versionist

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :auths, only: [:create]
   resources :kinds
   
-  api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.api+json; version=1"}) do
+  api_version(:module => "V1", :header => {:name => "X-Version", :value => "1.0"}) do
     resources :contacts do
       resource :kind, only: [:show]
       resource :kind, only: [:show], path: 'relationships/kind'
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     end
   end
 
-  api_version(:module => "V2", :header => {:name => "Accept", :value => "application/vnd.api+json; version=2"}) do
+  api_version(:module => "V2", :header => {:name => "X-Version", :value => "2.0"}) do
     resources :contacts do
       resource :kind, only: [:show]
       resource :kind, only: [:show], path: 'relationships/kind'
