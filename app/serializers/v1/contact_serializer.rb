7f95992b21b625e09118cffbbd879638b4c6aaf0
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-01-10 19:13:17 -0300

    Versioning with segment url

---
+++ app/serializers/v1/contact_serializer.rb
@@ -0,0 +1,34 @@
+module V1
+  class ContactSerializer < ActiveModel::Serializer
+    attributes :id, :name, :email, :birthdate #, :author
+
+    belongs_to :kind do
+      link(:related) { v1_contact_kind_url(object.id) }
+    end
+    has_many :phones do
+      link(:related) { v1_contact_phones_url(object.id) }
+    end
+
+    has_one :address do
+      link(:related) { v1_contact_address_url(object.id) }
+    end
+
+    # link(:self) { contact_url(object.id) }
+    # link(:kind) { kind_url(object.id) }
+
+    # def author
+    #   'Jimmy Ibanez'    
+    # end
+
+    meta do
+      { author: 'Jimmy ibanez' }
+    end 
+    
+    def attributes(*args)
+      h = super(*args)
+      # es ---> h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+      h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+      h
+    end
+  end
+end
