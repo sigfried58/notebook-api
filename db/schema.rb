b6245d277681ae0b9a2b5bab3e749148058b0292
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-01-14 19:48:22 -0300

    Add figaro, config database pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20180109235629) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "addresses", force: :cascade do |t|
     t.string "street"
     t.string "city"
-    t.integer "contact_id"
+    t.bigint "contact_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["contact_id"], name: "index_addresses_on_contact_id"
@@ -27,7 +30,7 @@ ActiveRecord::Schema.define(version: 20180109235629) do
     t.date "birthdate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "kind_id"
+    t.bigint "kind_id"
     t.index ["kind_id"], name: "index_contacts_on_kind_id"
   end
 
@@ -39,7 +42,7 @@ ActiveRecord::Schema.define(version: 20180109235629) do
 
   create_table "phones", force: :cascade do |t|
     t.string "number"
-    t.integer "contact_id"
+    t.bigint "contact_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["contact_id"], name: "index_phones_on_contact_id"
@@ -74,4 +77,7 @@ ActiveRecord::Schema.define(version: 20180109235629) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "addresses", "contacts"
+  add_foreign_key "contacts", "kinds"
+  add_foreign_key "phones", "contacts"
 end
