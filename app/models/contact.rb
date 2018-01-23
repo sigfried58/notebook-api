e08b3fed00414bc804b694535f9bed4edbb18f5b
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-01-23 17:22:05 -0300

    Validate presence of attributes in contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -12,6 +12,8 @@ class Contact < ApplicationRecord
   has_many :phones
   has_one :address
 
+  validates :name, :email, :birthdate, :kind, presence: true
+  
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address, update_only: true
   
