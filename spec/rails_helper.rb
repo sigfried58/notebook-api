e8964b2e9f4b380d1eb4b9344f66e003fb7a6ad1
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-01-26 22:24:24 -0300

    Config for test device auth token in controllers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,9 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  # Devise
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+
   # Shoulda Matchers
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
